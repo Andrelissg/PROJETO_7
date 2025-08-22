58cb657c635ed9c536e951284102e8e7a6e3b801
developerfabiocsilva@outlook.com <developerfabiocsilva@outlook.com>
2025-08-21 22:37:11 -0300

    estilizando pagina ListaDeTarefas, mobile first

--- proj-7/src/componentes/ListaDeTarefas.tsx
+++ proj-7/src/componentes/ListaDeTarefas.tsx
@@ -1,32 +1,20 @@
-import React from "react";
+import "../style/ListaDeTarefas.css";
 
 function ListaDeTarefas({ tarefas }) {
   if (tarefas.length === 0) {
-    return (
-      <p style={{ color: "#7152faff", fontStyle: "italic" }}>
-        Nenhuma tarefa cadastrada.
-      </p>
-    );
+    return <p className="sem-tarefas">Nenhuma tarefa cadastrada.</p>;
   }
 
   return (
-    <ul style={{ listStyle: "none", padding: 0 }}>
-      {tarefas.map((tarefa) => (
-        <li
-          key={tarefa.id}
-          style={{
-            backgroundColor: tarefa.concluida ? "#ac9efaff" : "#a396ebff",
-            textDecoration: tarefa.concluida ? "line-through" : "none",
-            padding: "10px",
-            marginBottom: "12px",
-            borderRadius: "12px",
-            boxShadow: "0 2px 5px rgba(51, 37, 243, 0.1)",
-          }}
-        >
-          {tarefa.titulo}
-        </li>
-      ))}
-    </ul>
+    <div className="todo-container">
+      <ul style={{ listStyle: "none", padding: 0 }}>
+        {tarefas.map((tarefa) => (
+          <li key={tarefa.id} className="todo-input">
+            {tarefa.titulo}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
