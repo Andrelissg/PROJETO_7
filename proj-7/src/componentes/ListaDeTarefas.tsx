eca6b890b29b7a6801c6fc49e89695eee3dd59bb
Marta_Gomes <martahonorato1919@gmail.com>
2025-08-25 10:02:23 -0300

    Adiciona componente ItemTarefa

--- proj-7/src/componentes/ListaDeTarefas.tsx
+++ proj-7/src/componentes/ListaDeTarefas.tsx
@@ -4,7 +4,7 @@ function ListaDeTarefas({ tarefas }) {
   if (tarefas.length === 0) {
     return <p className="sem-tarefas">Nenhuma tarefa cadastrada.</p>;
   }
-
+""
   return (
     <div className="todo-container">
       <ul style={{ listStyle: "none", padding: 0 }}>
